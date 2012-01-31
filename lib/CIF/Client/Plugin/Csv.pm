478f634c03362ce10e050059630bb1e697bfad6f
Wes Young <saxjazman@cpan.org>
2012-01-31 16:08:13 +0000

    Import of SAXJAZMAN/CIF-Client-0.02_05 from CPAN.
    gitpan-cpan-distribution: CIF-Client
    gitpan-cpan-version:      0.02_05
    gitpan-cpan-path:         SAXJAZMAN/cif/CIF-Client-0.02_05.tar.gz
    gitpan-cpan-author:       SAXJAZMAN
    gitpan-cpan-maturity:     developer

--- lib/CIF/Client/Plugin/Csv.pm
+++ lib/CIF/Client/Plugin/Csv.pm
@@ -9,6 +9,9 @@ sub write_out {
     my $config = shift;
     my $feed = shift;
     my @array = @{$feed->{'feed'}->{'entry'}};
+    
+    $config = $config->{'config'};
+    my $nosep = $config->{'csv_noseperator'};
     #my @header = keys(%{$array[0]});
     my @header;
     # skip things like arrays and hashrefs for now
@@ -20,8 +23,23 @@ sub write_out {
     my $body = '';
     foreach my $a (@array){
         delete($a->{'message'}); 
+        # there's no clean way to do this just yet
+        foreach (@header){
+            if($a->{$_} && !ref($a->{$_})){
+                # deal with , in the field
+                if($nosep){
+                    $a->{$_} =~ s/,/ /g;
+                    $a->{$_} =~ s/\s+/ /g;
+                } else {
+                    $a->{$_} =~ s/,/_/g;
+                }
+                # strip out non-ascii (typically unicode) chars
+                # there are better ways to do this, but this works for now
+                $a->{$_} =~ tr/\000-\177//cd;
+            }
+        }
         # the !ref() bits skip things like arrays and hashref's for now...
-        $body .= join(',',map { ($a->{$_} && !ref($a->{$_})) ? $a->{$_} : ''} @header)."\n";
+        $body .= join(',', map { ($a->{$_} && !ref($a->{$_})) ? $a->{$_} : ''} @header)."\n";
     }
     my $text = '# '.join(',',@header);
     $text .= "\n".$body;
