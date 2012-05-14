c0ee48077aff926249cf5075bdac30429ab207ea
Wes Young <saxjazman@cpan.org>
2012-05-14 15:06:34 +0000

    Import of SAXJAZMAN/CIF-Client-0.11_03 from CPAN.
    gitpan-cpan-distribution: CIF-Client
    gitpan-cpan-version:      0.11_03
    gitpan-cpan-path:         SAXJAZMAN/cif/CIF-Client-0.11_03.tar.gz
    gitpan-cpan-author:       SAXJAZMAN
    gitpan-cpan-maturity:     developer

--- lib/CIF/Client.pm
+++ lib/CIF/Client.pm
@@ -19,7 +19,7 @@ use URI::Escape;
 
 __PACKAGE__->mk_accessors(qw/apikey config/);
 
-our $VERSION = '0.11_02';
+our $VERSION = '0.11_03';
 $VERSION = eval $VERSION;  # see L<perlmodstyle>
 
 # Preloaded methods go here.
--- lib/CIF/Client/Plugin/Snort.pm
+++ lib/CIF/Client/Plugin/Snort.pm
@@ -1,6 +1,9 @@
 package CIF::Client::Plugin::Snort;
 use base 'CIF::Client::Plugin::Output';
 
+use warnings;
+use strict;
+
 use Snort::Rule;
 use Regexp::Common qw/net/;
 
@@ -14,13 +17,14 @@ sub write_out {
     $config = $config->{'config'};
     
     # allow override of snort rule params
-    my $tag = confor($config, 'snort_tag', undef);
-    my $pri = confor($config, 'snort_priority', undef);
-    my $sid = confor($config, 'snort_startsid', 1);
-    my $thresh = confor($config,'snort_threshold', 'type limit,track by_src,count 1,seconds 3600');
-    my $classtype = confor($config, 'snort_classtype', undef);
-    my $srcnet = confor($config, 'snort_srcnet', 'any');
-    my $srcport = confor($config, 'snort_srcport', 'any');
+    my $tag         = confor($config, 'snort_tag', undef);
+    my $pri         = confor($config, 'snort_priority', undef);
+    my $sid         = confor($config, 'snort_startsid', 1);
+    my $thresh      = confor($config, 'snort_threshold', 'type limit,track by_src,count 1,seconds 3600');
+    my $classtype   = confor($config, 'snort_classtype', undef);
+    my $srcnet      = confor($config, 'snort_srcnet', 'any');
+    my $srcport     = confor($config, 'snort_srcport', 'any');
+    my $msg_prefix  = confor($config, 'snort_msg_prefix','');
 
     my $rules = '';
 
@@ -39,25 +43,82 @@ sub write_out {
             $priority = 9 if(/high/);
         }
 
+        my $dstnet = 'any';
+        my $dstport = 'any';
+        my $urlhost = undef;
+        my ($urlport, $urlfile);
+
+        if (isipv4($_->{'address'})) {
+            $dstnet = $_->{'address'};
+            $dstport = $portlist;   
+        }
+        else {
+            ($urlhost, $urlport, $urlfile) = ishttpurl($_->{'address'});
+            if (defined($urlhost)) {
+                my $urlisip = isipv4($urlhost);
+                $_->{'protocol'} = 6; # TCP by definition
+                $dstnet = ($urlisip ? $urlhost : 'any'); # $EXTERNAL_NET?
+                $dstport = $urlport || '$HTTP_PORTS';
+            }
+            else {
+                $rules .= "### sorry. not sure what to do with address: " . $_->{'address'} . " so i'm skipping this one.\n\n";
+                next;
+            }
+        }
+
         my $r = Snort::Rule->new(
             -action => 'alert',
             -proto  => translate_proto($_->{'protocol'}),
             -src    => $srcnet,
             -sport  => $srcport,
-            -dst    => $_->{'address'},
-            -dport  => $portlist,
+            -dst    => $dstnet,
+            -dport  => $dstport,
             -dir    => '->',
         );
-        $r->opts('msg',$_->{'restriction'}.' - '.$_->{'impact'}.' '.$_->{'description'});
+
+        my $reference = make_snort_ref($_->{'alternativeid'});
+
+        $r->opts('msg',$msg_prefix . $_->{'restriction'}.' - '.$_->{'impact'}.' '.$_->{'description'});
         $r->opts('threshold', $thresh) if $thresh;
         $r->opts('tag', $tag) if $tag;
         $r->opts('classtype', $classtype) if $classtype;
         $r->opts('sid', $sid++);
-        $r->opts('reference',$_->{'alternativeid'}) if($_->{'alternativeid'});
+        $r->opts('reference',$reference) if($reference);
         $r->opts('priority', $pri || $priority);
-        $rules .= $r->string()."\n";
+
+        #alert tcp $HOME_NET any -> $EXTERNAL_NET $HTTP_PORTS (Msg: "Mal_URI
+        #www.badsite.com/malware.pl"; flow: to_server, established;
+        #content:"Host|3A| www.basesite.com"; nocase;
+        #content:"/malware.pl"; http_uri; nocase; sid:23424234;)
+
+        if ($urlhost) {
+            $r->opts('flow', 'to_server');
+            if (!isipv4($urlhost)) {
+                $r->opts('content', 'Host|3A| ' . escape_content($urlhost));
+                $r->opts('http_header');
+                $r->opts('nocase');
+            }
+            if ($urlfile) {
+                $r->opts('content', escape_content($urlfile));
+                $r->opts('http_uri');
+                $r->opts('nocase');
+            }
+        }
+    
+        $rules .= "# " . $_->{'address'} . "\n";
+        $rules .= $r->string()."\n\n";
     }
     return $rules;
+}
+
+sub isipv4 {
+    my ($i, $m) = (shift, 32);
+    ($i, $m) = split('/', $i) if ($i =~ /\//);
+    return 1 if ( 
+        ($i =~ /^0*([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])(\.0*([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])){3}$/) &&
+        ($m > 0 && $m < 33)
+        );
+    return 0;
 }
 
 sub translate_proto {
@@ -80,5 +141,45 @@ sub confor {
 		return ref($conf->{$name} eq "ARRAY") ? join(', ', @{$conf->{$name}}) : $conf->{$name};
 	}
 	return $def;
+}
+
+sub ishttpurl {
+    my $x = shift;
+
+    return (undef, undef, undef) unless $x;
+
+    # it only makes sense to try to look for http: urls
+    # https will be encrypted, ftp doesnt contain header fields to trigger on, etc
+
+    if ($x =~ /http:\/\/([^\/]+)[\/]{0,1}(.*)/) {
+        my ($h, $p) = split(':', $1);
+        my $d = ($2 ? '/'.$2 : '');
+        return ($h, $p, $d);
+    }
+    return (undef, undef, undef); 
+}
+
+sub make_snort_ref {
+    my $r = shift;
+    return undef unless defined($r);
+    if ($r =~ /(https?):\/\/(.*)/) {
+        return "url," . $2 if ($1 eq "http");
+        return "urlssl," . $2;
+    }
+    return undef;
+}
+
+# http://manual.snort.org/node32.html#SECTION00451000000000000000
+#Note:  
+#Also note that the following characters must be escaped inside a content rule:
+#
+#    ; \ "
+    
+sub escape_content {
+    my $x = shift;
+    $x =~ s/\\/\\\\/gi;
+    $x =~ s/;/\\;/gi;
+    $x =~ s/\"/\\"/gi;
+    return $x;
 }
 1;
